7620f6f13700d68407d50e34a634b7189a3b3a96
1s00b1n <1s00b1n526@gmail.com>
2025-08-18 10:23:35 +0900

    로컬 변경사항 임시저장

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template, jsonify, request, url_for
+from pymongo import MongoClient
 app = Flask(__name__)
+
+uri = "mongodb+srv://1s00b1n526:urzcH2690ovTy7xS@cluster0.lows1qr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&tlsAllowInvalidCertificates=true"
+client = MongoClient(uri)
+db = client.junglejungle
 
 # 루트 URL('/')에 대한 라우트를 정의합니다.
 # 사용자가 웹사이트의 메인 페이지에 접속할 때 이 함수가 실행됩니다.
@@ -11,7 +15,6 @@ def index():
 # '/game/title' URL에 대한 라우트를 정의합니다.
 @app.route('/game/title')
 def game_title():
-
     return render_template('game_title.html')
 
 # '/game/컷번호' URL에 대한 라우트를 정의합니다. cut_id : 0~
@@ -19,11 +22,40 @@ def game_title():
 def game_cut(cut_id):
     return render_template(f'cut{cut_id}.html')
 
+    # '/game/ending' URL에 대한 라우트를 정의합니다.
+@app.route('/game/ending')
+def game_ending():
+    return render_template('game_ending.html')
 
+# playername 받아서 저장
+@app.route('/game', methods=['POST'])
+def save_playerName():
+    receive_playerName = request.form.get('playerName')
+    playerName = {'playerName': receive_playerName} 
+    db.playerName.insert_one(playerName)
+    return jsonify({'result': 'success'})
 
+# playername 불러오기
+@app.route('/game', methods=['GET'])
+def load_playerName():
+    result = list(db.playerName.find({}, {'_id': 0}))
+    return jsonify({'result': 'success', 'playerName': result})
 
+# commnet 저장
+@app.route('/game/ending/comment', methods=['POST'])
+def save_comment():
+    if request.method == 'POST':
+        receive_comment = request.form.get('playerComment')
+        receive_date = request.form.get('Date')
+        comment_data = {'playerComment': receive_comment, 'Date': receive_date}  # +유저 정보 추가
+        db.playerComment.insert_one(comment_data)
+        return jsonify({'result': 'success'})
 
-
+# comment 불러오기
+@app.route('/game/ending/comment', methods=['GET'])
+def load_comment():
+    result = list(db.playerComment.find({}, {'_id': 0}))
+    return jsonify({'result': 'success', 'playerComment': result, 'Date': result})
 
 if __name__ == '__main__':
 
